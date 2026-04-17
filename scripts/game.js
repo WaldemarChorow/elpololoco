2ee782e6f65182f52c81d7694079af34b588bdfa
WaldemarChorow <chorow.w@gmail.com>
2026-04-17 08:42:55 +0200

    Add game structure and character classes with initial implementation

--- scripts/game.js
+++ scripts/game.js
@@ -1,14 +1,11 @@
 let canvas;
-let character = new Image();
+let world;
 
 function init() {
     canvas = document.getElementById('canvas');
-    ctx = canvas.getContext('2d');
-    character.src = '../assets/img/2_character_pepe/2_walk/W-21.png';
-
-    setTimeout(() => {
-    ctx.drawImage(character, 20, 160, 150, 260);
-
-    }, 1000);
+    world = new World(canvas);
+        
+    console.log('My Character is', world.character);
+    
 }
 
