02c6604a0bafa28b55612cd9edfee9b11e4829c2
WaldemarChorow <chorow.w@gmail.com>
2026-04-17 07:11:10 +0200

    Refactor code structure for improved readability and maintainability

---
+++ scripts/game.js
@@ -0,0 +1,14 @@
+let canvas;
+let character = new Image();
+
+function init() {
+    canvas = document.getElementById('canvas');
+    ctx = canvas.getContext('2d');
+    character.src = '../assets/img/2_character_pepe/2_walk/W-21.png';
+
+    setTimeout(() => {
+    ctx.drawImage(character, 20, 160, 150, 260);
+
+    }, 1000);
+}
+
